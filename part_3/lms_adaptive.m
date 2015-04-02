ad79556a9d8fa335ee1e2fdc39c485089f5226ee
Thomas <txl11@imperial.ac.uk>
2015-04-02 17:28:45 +0100

    Progress with 3.2, and tidy up on 3.1

--- part_3/lms_adaptive.m
+++ part_3/lms_adaptive.m
@@ -1,44 +1,42 @@
-function [ w_est, error_sq ] = lms_adaptive(x, order, step_method, ro )
+function [ w_est, error_sq ] = lms_adaptive(wgn, x, step_method, ro, alpha)
 
 N = length(x);
 
-w_est = zeros(order, N);
+w_est = zeros(1, N);
 error = zeros(N, 1);
 x_est = zeros(N, 1);
 
 step = 0;
 mu = 0;
 
-alpha = 0.1;
-
-for n = order+2:N
-    
-    x_est(n) = w_est(:, n)' * x(n-1);
+for n = 3:N
+    %% Standard LMS Algorithm
+    x_est(n) = w_est(:, n)' * wgn(n-1);
     
     error(n) = x(n) - x_est(n);
     
-    w_est(:, n+1) = w_est(:, n) + mu*error(n)*x(n);
+    w_est(:, n+1) = w_est(:, n) + mu*error(n)*wgn(n-1);
     
+    %% GASS Algorithms
     % Benveniste
     if strcmp(step_method, 'benveniste') 
-    step = (1 - mu*(x(n-2)^2))*step + error(n-1)*x(n-2);
+    step = (1 - mu*(wgn(n-2)^2))*step + error(n-1)*wgn(n-2);
     
     %Ang & Farhang
     elseif strcmp(step_method, 'ang')
-    step = alpha*step + error(n-1)*x(n-2);
+    step = alpha*step + error(n-1)*wgn(n-2);
     
     % Matthews & Xie
     elseif strcmp(step_method, 'matthews')
-    step = error(n-1)*x(n-2);
-
+    step = error(n-1)*wgn(n-2);
+    
+    % Standard LMS
+    else
+    step = 0;
     end
     
     mu = mu + (ro * error(n) * x(n-1) * step);
-    mu
-    pause
-%     disp(mu)
-%     mu = 0.1;
-    
+
 end
 
 error_sq = error.^2;
--- part_3/part_3_2_a.m
+++ part_3/part_3_2_a.m
@@ -4,23 +4,37 @@ common.init
 
 N = 1000;
 N_IT = 100;
-VAR_PROC = 0.5;
+VAR_PROC = 0.25;
 ORDER = 1;
 
 x_mat = zeros(N, N_IT);
-
+wgn_mat = zeros(N, N_IT);
 for i = 1:N_IT
+    
     wgn = randn(N, 1)*sqrt(VAR_PROC);
+    
+    wgn_mat(:, i) = wgn;
 
     x = zeros(N, 1);
+    diff_x = x;
 
-    % Generate the signal
+%     % Generate the signal
     x(1) = wgn(1);
 
+    % Generate the signal
+%     x(1:2) = wgn(1:2);
+
     for n = 2:N
+%         x(n) = x(n-1) * 0.9 + wgn(n);
         x(n) = wgn(n) + 0.9*wgn(n-1);
     end
     
+%     wgn = randn(N, 1)*sqrt(VAR_PROC);
+% 
+%     x = zeros(N, 1);
+% 
+% 
+%     
     x_mat(:, i) = x;
 end
 
@@ -28,14 +42,19 @@ end
 error_tot = zeros(N, 1);
 w_est_tot = zeros(ORDER, N+1);
 
-figure
-hold all
-    
 for i = 1:N_IT
-    [ w_est, ~] = lms_adaptive(x_mat(:, i), 1, 'matthews', 0.01);
+    [ w_est, error] = lms_adaptive(wgn_mat(:, i), x_mat(:, i), 'benveniste', 0.01);
     w_est_tot = w_est_tot + w_est;
-    plot(w_est)
+    error_tot = error + error_tot;
+    %     plot(w_est)
 end
 
 w_est_tot = w_est_tot/N_IT;
-plot(0.9 - w_est_tot)
+w_est_shift = 0.9 - w_est_tot;
+
+plot(w_est_shift)
+ 
+% error_tot = error_tot/N_IT;
+% figure
+% plot(error_tot)
+
