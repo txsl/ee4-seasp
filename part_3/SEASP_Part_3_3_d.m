3134261a9f9b2bcccd8de1cd41832edb663a258c
Thomas <txl11@imperial.ac.uk>
2015-04-04 22:58:50 +0100

    Part 3_3_d now producing something which probably works. This is to be returned to after SE work done (so we know about windowing)

--- part_3/SEASP_Part_3_3_d.m
+++ part_3/SEASP_Part_3_3_d.m
@@ -10,7 +10,7 @@ sine_wave = sin(100*pi*(1:N)/fs)';
 noise_sig = zeros(N, N_IT);
 
 for i = 1:N_IT
-    noise_sig(:, i) = 3*randn(N, 1) + sine_wave;
+    noise_sig(:, i) = randn(N, 1) + 2*sine_wave;
 end
 
 % tic
@@ -18,14 +18,9 @@ end
 %     [ ~, ~, errors(:, i) ] = lms(noise_sig(:, i), Cz, 8, .01, 0);
 % end
 % toc
-%% Step Sizes
+%% Step Sizes - Generate Data!
 
-% mus = [ 0.001 0.01 0.1 ];
-mus = 0.01;
-
-figure;
-% subplot(2, 2, 1);
-specgram(Cz, [], fs)
+mus = [ 0.001 0.01 0.1 ];
 
 mus_errors = cell(length(mus), 1);
 
@@ -34,26 +29,23 @@ for j = 1:length(mus)
     errors = zeros(N, N_IT);
     
     m = mus(j)
+    
     for i = 1:N_IT
         [ ~, ~, errors(:, i) ] = lms(noise_sig(:, i), Cz, 10, m, 0);
     end
-    mus_errors{j} = errors;
-%     save(sprintf('mu_%f.mat', mus(j)), mean(errors,2));
-%     subplot(2, 2, j)
-figure
-    specgram(mean(errors, 2), [], fs)
-%     title(sprintf('\\mu = %f', mus(j)))
 end
 
-%% Filter Orders
-% 
+save('mu.mat', 'mus_errors');
+
+%% Filter Orders - Generate Data!
+
 % orders = [ 2 5 10 15 20 ];
 % 
-% % figure;
-% % subplot(2, 2, 1);
-% % specgram(Cz, [], fs)
-% 
-% orders_errors = cell(length(orders), 1);
+% figure;
+% subplot(2, 3, 1);
+% specgram(Cz, [], fs)
+% % 
+% % orders_errors = cell(length(orders), 1);
 % for j = 1:length(orders)
 %     
 %     errors = zeros(N, N_IT);
@@ -61,15 +53,31 @@ end
 %     for i = 1:N_IT
 %         [ ~, ~, errors(:, i) ] = lms(noise_sig(:, i), Cz, o, 0.01, 0);
 %     end
-%     orders_errors{j} = errors;
-% %     subplot(2, 2, j)
-% %     specgram(mean(orders, 2), [], fs)
-% %     title(sprintf('\\mu = %f', mus(j)))
+% %     orders_errors{j} = errors;
+%     subplot(2, 3, j+1)
+%     specgram(mean(orders, 2), [], fs)
+%     title(sprintf('M = %i', orders(j)))
 % end
 
-%% Save
+%% Step Sizes - Plot Data
 
-save('ecg_noise_removal.mat', 'orders_errors', 'mus_errors');
+load('mu.mat');
+
+figure;
+subplot(2, 2, 1);
+specgram(Cz, [], fs)
+title('Original Signal')
+
+for j = 1:length(mus)
+    
+    subplot(2, 2, j+1)
+
+    specgram(mean(mus_errors{j}, 2), [], fs)
+    title(sprintf('$\mu$ = %f', mus(j)))
+end
+
+%% Filter Orders - Plot Data!
+
 
 %% Old
 
