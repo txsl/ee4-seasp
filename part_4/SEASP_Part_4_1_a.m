9a056cd955487ce0c920b0a9a6e396489ec952ef
Thomas Lim <txl11@ic.ac.uk>
2015-04-05 00:48:03 +0100

    CLMS of part 4 working

--- part_4/SEASP_Part_4_1_a.m
+++ part_4/SEASP_Part_4_1_a.m
@@ -1,12 +1,18 @@
 
-addpath('/../')
+addpath('../')
 common.init
+
+%% Setup
 
 N = 1000;
 N_IT = 100;
+MU = 0.01;
+ORDER = 3;
 
 b1 = 1.5 + 1j;
 b2 = 2.5 - 0.5j;
+
+%% Generate Signals
 
 x = zeros(N, N_IT);
 y = zeros(N, N_IT);
@@ -21,8 +27,23 @@ for i = 1:N_IT
     end
 end
 
-h_est = 
+%% CLMS
 
+h_est = zeros(ORDER, N, N_IT);
+
+for i = 1:N_IT
+    [ h_est(:, :, i) , ~, ~ ] = clms(x(:,i), y(:,i), ORDER, MU); 
+end
+
+
+plot(mean(real(h_est), 3)')
+
+figure
+plot(mean(imag(h_est), 3)')
+
+%% ACLMS
+
+h_est = zeros(ORDER, N, N_IT);
 for i = 1:N_IT
     
 end
--- part_4/clms.m
+++ part_4/clms.m
@@ -1,20 +1,20 @@
-function [ h_est, x_est, error ] = clms(x, d, order, mu )
+function [ h_est, y_est, error ] = clms(x, d, order, mu)
 
 N = length(x);
 
 h_est = zeros(order, N);
 error = zeros(N, 1);
-x_est = zeros(N, 1);
+y_est = zeros(N, 1);
 
-for n = order+1:N
+for n = order+1:N-1
     
     x_n = flipud(x(n-order:n-1));
     
-    x_est(n) = h_est(:, n)' * x_n;
+    y_est(n) = h_est(:, n)' * x_n;
     
-    error(n) = d(n) - x_est(n);
+    error(n) = d(n) - y_est(n);
     
-    h_est(:, n+1) = h_est(:, n) + mu*error(n)*x_n;
+    h_est(:, n+1) = h_est(:, n) + mu*conj(error(n))*x_n;
     
 end
 
