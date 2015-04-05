539c2fc810ef13661154ff16db82bd75b0c71762
Thomas <txl11@imperial.ac.uk>
2015-04-05 14:07:02 +0100

    3_1_f code updated

--- part_3/SEASP_Part_3_1_f.m
+++ part_3/SEASP_Part_3_1_f.m
@@ -12,45 +12,32 @@ w = [0.1; 0.8];
 x_mat = zeros(N, N_IT);
 
 for i = 1:N_IT
-    wgn = randn(N, 1)*sqrt(VAR_PROC);
-
-    x = zeros(N, 1);
-    diff_x = x;
 
     % Generate the signal
-    x(1:2) = wgn(1:2);
+    x_mat(:, i) = filter(1, [1 -0.1 -0.8], randn(N, 1)*sqrt(VAR_PROC));
 
-    for n = 3:N
-        x(n) = fliplr(x(n-2:n-1)') * w + wgn(n);
-    end
-    
-    x_mat(:, i) = x;
 end
 
 figure
 hold on
-colours = hsv(12);
+colours = distinguishable_colors(8);
 n = 1;
 
 for mu = [ 0.01 0.05 ]
     
     for leak = [0 0.2 0.5 0.8]
-        error_tot = zeros(N, 1);
-        w_est_tot = zeros(ORDER, N+1);
+        error_tot = zeros(N, N_IT);
+        w_est_tot = zeros(ORDER, N+1, N_IT);
         
         for i = 1:N_IT
-            	[ w_est, ~, error_sq ] = lms(x_mat(:, i), x_mat(:, i), ORDER, mu, leak);
-                error_tot = error_tot + error_sq;
-                w_est_tot = w_est_tot + w_est;
+            [ w_est_tot(:, :, i), ~, error_tot(:, i) ] = lms(x_mat(:, i), x_mat(:, i), ORDER, mu, leak);
         end
         
-        error_tot = error_tot/N_IT;
-        error_db = 10*log10(error_tot);
+        error_db = 10*log10(mean(error_tot.^2, 2));
         plot(error_db, 'DisplayName', sprintf('mu = %f, gamma = %f', mu, leak), 'color', colours(n,:))
         n = n + 1;
         
-        w_est_tot = w_est_tot/N_IT;
-        w_est_tot = mean(w_est_tot(:, 800:end), 2);
+        w_est_tot = mean(mean(w_est_tot(:, 800:end, :), 3), 2);
         
         fprintf('mu=%f, leak=%f, a1=%f, a2=%f\n', mu, leak, w_est_tot(1), w_est_tot(2));
     end
@@ -61,3 +48,5 @@ grid on;
 title('LMS filter with different step sizes and leak parameters');
 xlabel('Iteration')
 ylabel('Squared Prediction Error (dB)')
+common.set_graph_params
+
