2a4b525eaa6335a3d27f254237867fc1192b9bd4
Thomas <txl11@imperial.ac.uk>
2015-04-05 22:40:19 +0100

    4.1 a and b should be complete now

--- part_4/SEASP_Part_4_1_a.m
+++ part_4/SEASP_Part_4_1_a.m
@@ -7,7 +7,7 @@ common.init
 N = 1000;
 N_IT = 100;
 MU = 0.01;
-ORDER = 3;
+ORDER = 1;
 
 b1 = 1.5 + 1j;
 b2 = 2.5 - 0.5j;
@@ -30,34 +30,60 @@ end
 %% CLMS
 
 h_est = zeros(ORDER, N, N_IT);
+error = zeros(N, N_IT);
 
 for i = 1:N_IT
-    [ h_est(:, :, i), ~, ~ ] = clms(x(:,i), y(:,i), ORDER, MU); 
+    [ h_est(:, :, i), ~, error(:, i) ] = clms(x(:,i), y(:,i), ORDER, MU); 
 end
 
-
-plot(mean(real(h_est), 3)')
-
 figure
-plot(mean(imag(h_est), 3)')
+plot(10*log10(mean(abs(error), 2)).^2)
+title('CLMS Error for WLMA(1) process')
+xlabel('Iteration')
+ylabel('Learning Curve (dB)')
+common.set_graph_params
+
+figure;
+plot(1:N, mean(real(h_est), 3)', 1:N, mean(imag(conj(h_est)), 3)');
+title('Estmated Weights using CLMS');
+xlabel('Iteration');
+ylabel('Estimated Weight');
+legend('$\mathfrak{R}{\mathbb{h^H}}$', '$\mathfrak{I}{\mathbb{h^H}}$')
+common.set_graph_params
+
 
 %% ACLMS
 
 h_est = zeros(ORDER, N, N_IT);
 g_est = zeros(ORDER, N, N_IT);
+error = zeros(N, N_IT);
+
 for i = 1:N_IT
-    [ h_est(:, :, i), g_est(:, :, i), ~, ~ ] = aclms(x(:,i), y(:,i), ORDER, MU); 
+    [ h_est(:, :, i), g_est(:, :, i), ~, error(:, i) ] = aclms(x(:,i), y(:,i), ORDER, MU); 
 end
 
-figure
+figure;
+plot(10*log10(mean(abs(error), 2)).^2)
+title('ACLMS Error for WLMA(1) process')
+xlabel('Iteration')
+ylabel('Learning Curve (dB)')
+common.set_graph_params
 
-plot(mean(real(h_est), 3)')
+figure;
+plot(1:N, mean(real(h_est), 3)', 1:N, mean(imag(conj(h_est)), 3)', 1:N, mean(real(g_est), 3)', 1:N, mean(imag(conj(g_est)), 3)');
+title('Estmated Weights using ACLMS');
+xlabel('Iteration');
+ylabel('Estimated Weight');
+legend('$\mathfrak{R}\{\mathbb{h^H}\}$', '$\mathfrak{I}\{\mathbb{h^H}\}$', '$\mathfrak{R}\{\mathbb{g^H}\}$', '$\mathfrak{I}\{\mathbb{g^H}\}$')
+common.set_graph_params
 
-figure
-plot(mean(imag(h_est), 3)')
-
-figure
-plot(mean(real(g_est), 3)')
-
-figure
-plot(mean(imag(g_est), 3)')
+% plot(mean(real(h_est), 3)')
+% 
+% figure
+% plot(mean(imag(h_est), 3)')
+% 
+% figure
+% plot(mean(real(g_est), 3)')
+% 
+% figure
+% plot(mean(imag(g_est), 3)')
--- part_4/SEASP_Part_4_1_b.m
+++ part_4/SEASP_Part_4_1_b.m
@@ -25,9 +25,9 @@ for i = 1:3
 
     subplot(1, 3, i)
     scatter(real(v(:, i)), imag(v(:, i)), 'marker', '.');
-    title(titles{i});
-    xlabel('$ \mathfrak{R} a $');
-    ylabel('$ \mathfrak{I} $');
+    title(sprintf('Circularity Plot: %s', titles{i}));
+    xlabel('$ \mathfrak{R}{v[n]} $');
+    ylabel('$ \mathfrak{I}{v[n]} $');
     
     low = min(min(real(v(:, i))), min(imag(v(:, i))));
     high = max(max(real(v(:, i))), max(imag(v(:, i))));
@@ -39,25 +39,37 @@ end
 %% Let's try modelling it
 
 figure;
-hold on;
 
-plots = { 'oy', '+m', '*c', '.r', 'xg', 'sb' };
+plots = { 'or', '+b' };
+
+ORDER_TOP = 30;
 
 for w = 1:3
-    clms_errs = zeros(20, 1);
-    aclms_errs = zeros(20, 1);
-    for i = 1:20
+    
+    clms_errs = zeros(ORDER_TOP, 1);
+    aclms_errs = zeros(ORDER_TOP, 1);
+    
+    for i = 1:ORDER_TOP
         [ ~, ~, error ] = clms(v(:, w), v(:, w), i, 0.01);
-        clms_errs(i) = mean(abs(error).^2);
+        clms_errs(i) = 10*log10(mean(abs(error).^2));
         
-        [ ~, ~, ~, ~ ] = aclms(v(:, w), v(:, w), i, 0.01);
-        aclms_errs(i) = mean(abs(error).^2);
+        [ ~, ~, ~, error ] = aclms(v(:, w), v(:, w), i, 0.01);
+        aclms_errs(i) = 10*log10(mean(abs(error).^2));
     end
-    plot(clms_errs, plots{w})
-    plot(aclms_errs, plots{w+3})
+    
+    subplot(1, 3, w)
+    hold on;
+    
+    plot(clms_errs, plots{1})
+    plot(aclms_errs, plots{2})
+    
+    xlabel('Model Order')
+    ylabel('Mean Squared Error (dB)')
+    title(sprintf('Prediction Error: %s', titles{w}));
+    legend('CLMS Model', 'ACLMS Model');
+    
+    common.set_graph_params
 end
-
-legend('CLMS - Low Speed Wind', 'ACLMS - Low Speed Wind', 'CLMS - Med. Speed Wind', 'ACLMS - Med. Speed Wind', 'CLMS - Hi. Speed Wind', 'ACLMS - Hi. Speed Wind');
 
 
 
