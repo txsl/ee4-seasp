2a68a42b2ae413f8574c3daaee1dfe34ce50b7d7
Thomas Lim <txl11@ic.ac.uk>
2015-04-05 04:46:19 +0100

    Slow step forwards on 4.2.b

--- part_4/SEASP_Part_4_2_b.m
+++ part_4/SEASP_Part_4_2_b.m
@@ -4,3 +4,17 @@ common.init
 
 SEASP_Part_4_2_a
 
+
+MU = 0.01;
+
+[ h_est, ~, ~ ] = clms(y, y, 1, MU);
+
+H = zeros(1024, 1500);
+
+for n = 1:1500
+    [ h, ~ ] = freqz(1, [1; h_est(:, n)], 1024);
+    H(:,n) = abs(h).^2;
+end
+
+medianH = 50*median(median(H));
+H(H > medianH) = medianH;
