cd17d7cea7c9434a779c42f20fa229a3168ae9d8
Thomas <txl11@imperial.ac.uk>
2015-04-05 13:43:47 +0100

    Part_3_1 code all updated and working nicely

--- part_3/SEASP_Part_3_1.m
+++ part_3/SEASP_Part_3_1.m
@@ -1,5 +1,5 @@
 
-addpath('/../')
+addpath('../')
 common.init
 
 N = 1000;
@@ -7,76 +7,61 @@ N_IT = 100;
 VAR_PROC = 0.25;
 ORDER = 2;
 
-w = [0.1; 0.8];
 
 %% 3_1_b
 
 mu_1 = 0.01;
 mu_2 = 0.05;
  
-error_tot_mu_5 = zeros(N, 1);
-w_est_tot_mu_5 = zeros(ORDER, N+1);
+error_tot_mu_5 = zeros(N, N_IT);
+w_est_tot_mu_5 = zeros(ORDER, N+1, N_IT);
 
-error_tot_mu_1 = zeros(N, 1);
-w_est_tot_mu_1 = zeros(ORDER, N+1);
+error_tot_mu_1 = zeros(N, N_IT);
+w_est_tot_mu_1 = zeros(ORDER, N+1, N_IT);
 
 start = 0;
 for i = 1:N_IT
-
     wgn = randn(N, 1)*sqrt(VAR_PROC);
-
-    x = zeros(N, 1);
-    diff_x = x;
-
+    
     % Generate the signal
-    x(1:2) = wgn(1:2);
+    x = filter(1, [1 -0.1 -0.8], randn(N, 1)*sqrt(VAR_PROC));
 
-    for n = 3:N
-        x(n) = fliplr(x(n-2:n-1)') * w + wgn(n);
-    end
+	[ w_est_tot_mu_5(:, :, i), ~, error_tot_mu_5(:, i) ] = lms(x, x, ORDER, 0.05, 0);
 
-	[ w_est, ~, error_sq ] = lms(x, x, ORDER, 0.05, 0);
-    error_tot_mu_5 = error_tot_mu_5 + error_sq;
-    w_est_tot_mu_5 = w_est_tot_mu_5 + w_est;
-
-    [ w_est, ~, error_sq ] = lms(x, x, ORDER, 0.01, 0);
-    error_tot_mu_1 = error_tot_mu_1 + error_sq;
-    w_est_tot_mu_1 = w_est_tot_mu_1 + w_est;
+    [ w_est_tot_mu_1(:, :, i), ~, error_tot_mu_1(:, i) ] = lms(x, x, ORDER, 0.01, 0);
     
     if start == 0
-        plot(1:N, 10*log10(error_tot_mu_1), 1:N, 10*log10(error_tot_mu_5))
-        legend('\mu = 0.01', '\mu = 0.05')
-        grid on;
+        plot(1:N, 10*log10(error_tot_mu_1(:,1).^2), 1:N, 10*log10(error_tot_mu_5(:,1).^2))
+        legend('$\mu$ = 0.01', '$\mu$ = 0.05')
+        common.set_graph_params
         title('LMS filter with differing step sizes');
         xlabel('Iteration')
         ylabel('Squared Prediction Error (dB)');
         start = 1;
     end
-    
 end
 
-% Average Error (for each iteration)
-error_tot_mu_5 = error_tot_mu_5/N_IT;
-error_tot_mu_1 = error_tot_mu_1/N_IT;
+% Square the error
+error_tot_mu_5 = error_tot_mu_5.^2;
+error_tot_mu_1 = error_tot_mu_1.^2;
 
 % Turn error to dB
-error_tot_mu_5_db = 10*log10(error_tot_mu_5);
-error_tot_mu_1_db = 10*log10(error_tot_mu_1);
+error_tot_mu_5_db = 10*log10(mean(error_tot_mu_5, 2));
+error_tot_mu_1_db = 10*log10(mean(error_tot_mu_1, 2));
 
 figure
 plot(1:N, error_tot_mu_1_db, 1:N, error_tot_mu_5_db)
-legend('\mu= 0.01', '\mu = 0.05')
-grid on;
+legend('$\mu$ = 0.01', '$\mu$ = 0.05')
+common.set_graph_params
 title('Mean Error of 100 iterations of LMS filter');
 xlabel('Iteration')
 ylabel('Squared Prediction Error (dB)')
 
-
 %% 3_1_c
 
 % EMSE Calculations
-emse_mu_5 = mean(error_tot_mu_5(800:end)) - VAR_PROC;
-emse_mu_1 = mean(error_tot_mu_1(800:end)) - VAR_PROC;
+emse_mu_5 = mean(mean(error_tot_mu_5(800:end, :), 2)) - VAR_PROC;
+emse_mu_1 = mean(mean(error_tot_mu_1(800:end, :), 2)) - VAR_PROC;
 
 % Misadjustment computation
 misadj_mu_5 = emse_mu_5/VAR_PROC
@@ -85,9 +70,6 @@ misadj_mu_1 = emse_mu_1/VAR_PROC
 
 %% 3_1_d
 % Average filter coefficient values - steady state
-w_est_tot_mu_5 = w_est_tot_mu_5/N_IT;
-w_est_tot_mu_1 = w_est_tot_mu_1/N_IT;
-
-w_est_ss_mu_5 =  mean(w_est_tot_mu_5(:, 800:end), 2)
-w_est_ss_mu_1 =  mean(w_est_tot_mu_1(:, 800:end), 2)
+w_est_ss_mu_5 =  mean(mean(w_est_tot_mu_5(:, 800:end, :), 3), 2)
+w_est_ss_mu_1 =  mean(mean(w_est_tot_mu_1(:, 800:end, :), 3), 2)
 
