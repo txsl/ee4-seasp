dcae81f7893f3540f4ed73fa76a3c4a3b0675085
Thomas Lim <txl11@ic.ac.uk>
2015-04-06 03:42:03 +0100

    init now saves breakpoints!

--- +common/init.m
+++ +common/init.m
@@ -1,4 +1,15 @@
+
+% Breakpoint saving thanks to http://stackoverflow.com/questions/12657219/how-to-restore-breakpoints-in-matlab-after-clear-all
+tmp = dbstatus;
+save('tmp.mat','tmp')
+
 close all; clear all; clc;
+
+load('tmp.mat')
+dbstop(tmp)
+
+clear tmp
+delete('tmp.mat')
 
 addpath('../libs')
 % addpath('../libs/voicebox')
