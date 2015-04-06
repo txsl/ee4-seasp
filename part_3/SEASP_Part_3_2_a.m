6724724d169b02ead323e28379ee0271fc7da7cc
Thomas Lim <txl11@ic.ac.uk>
2015-04-06 04:38:23 +0100

    Getting a pretty nice output for 3.2.a now!

--- part_3/SEASP_Part_3_2_a.m
+++ part_3/SEASP_Part_3_2_a.m
@@ -19,9 +19,9 @@ end
 
 %% Main plot comparing all methods, some rho values and the LMS
 
-methods = {  'ang', 'matthews'}; % 'benveniste', 'Benveniste', 
-method_names = containers.Map(methods, { 'Ang & Farhang', 'Matthews & Xie'});
-rhos = [ 0.002 ];
+methods = { 'benveniste', 'ang', 'matthews'}; % ,  
+method_names = containers.Map(methods, { 'Benveniste', 'Ang & Farhang', 'Matthews & Xie'});
+rhos = [ 0.01 0.001 ];
 no_lines = length(rhos)*length(methods) + 2;
 cols = distinguishable_colors(no_lines);
 col_idx = 1;
@@ -33,15 +33,14 @@ for mu = [0.01 0.1]
     w_est_tot = zeros(ORDER, N+1);
     
     for i=1:N_IT
-        [ w_est, ~ ] = lms_gass(wgn_mat(:, i), x_mat(:, i), 2, 'none', mu, 0, 0);
-%         [ w_est, ~, ~ ] = lms(wgn_mat(:, i), x_mat(:, i), ORDER, mu, 0);
+        [ w_est, ~ ] = lms_gass(wgn_mat(:, i), x_mat(:, i), ORDER, 'none', mu, 0, 0);
         w_est_tot = w_est_tot + w_est;
     end
     
     w_est_tot = w_est_tot/N_IT;
     w_est_shift = 0.9 - w_est_tot(1, :);
     plot(w_est_shift, 'Color', cols(col_idx, :));
-    leg{col_idx} = sprintf('LMS: \\mu=%f', mu);
+    leg{col_idx} = sprintf('LMS: \\mu=%0.3f', mu);
     col_idx = col_idx + 1;
 end
 
@@ -57,61 +56,61 @@ for method = methods
         w_est_tot = w_est_tot/N_IT;
         w_est_shift = 0.9 - w_est_tot(1, :);
         plot(w_est_shift, 'Color', cols(col_idx, :));
-        leg{col_idx} = sprintf('%s: \\rho = %f', method_names(method{1}), rho);
+        leg{col_idx} = sprintf('%s: \\rho = %0.3f', method_names(method{1}), rho);
         col_idx = col_idx + 1;
     end
 end
 
-xlim([1 N])
 legend(leg)
 title('GASS Methods tested across $\rho$ values, and LMS reference', 'interpreter', 'latex')
 xlabel('Iteration')
 ylabel('Weight Error ($\widehat{w}(n)$)', 'interpreter', 'latex')
-grid on
+xlim([0 350])
+common.set_graph_params
  
 %% Let's explore the value of alpha
 
-% alphas = [ 0.01 0.2 0.5 0.8 0.95 0.99 ];
-% cols = distinguishable_colors(length(alphas) + 2);
-% col_idx = 1;
-% figure;
-% hold on;
-% leg = cell(length(alphas) + 2, 1);
-% 
-% for mu = [0.01 0.1]
-%     w_est_tot = zeros(ORDER, N+1);
-%     
-%     for i=1:N_IT
-%         [ w_est, ~ ] = lms_gass(wgn_mat(:, i), x_mat(:, i), 'none', mu, 0, 0);
-%         w_est_tot = w_est_tot + w_est;
-%     end
-%     
-%     w_est_tot = w_est_tot/N_IT;
-%     w_est_shift = 0.9 - w_est_tot;
-%     plot(w_est_shift, 'Color', cols(col_idx, :));
-%     leg{col_idx} = sprintf('LMS: \\mu=%f', mu);
-%     col_idx = col_idx + 1;
-% end
-% 
-% for alpha = alphas
-%     w_est_tot = zeros(ORDER, N+1);
-%     
-%     for i = 1:N_IT
-%         [ w_est, ~ ] = lms_gass(wgn_mat(:, i), x_mat(:, i), 'ang', 0, 0.001, alpha);
-%         w_est_tot = w_est_tot + w_est;
-%     end
-%     
-%     w_est_tot = w_est_tot/N_IT;
-%     w_est_shift = 0.9 - w_est_tot;
-%     plot(w_est_shift, 'Color', cols(col_idx, :));
-%     leg{col_idx} = sprintf('\\alpha = %f', alpha);
-%     col_idx = col_idx + 1;
-% end
-% 
-% xlabel('Iteration')
-% ylabel('Weight Error ($\widehat{w}(n)$)', 'interpreter', 'latex')
-% title('Ang \& Farhang method for varying $\alpha$ values, and LMS reference', 'interpreter', 'latex')
-% xlim([1 N])
-% legend(leg)
-% grid on
+alphas = [ 0.01 0.5 0.8 0.9 ];
+cols = distinguishable_colors(length(alphas) + 2);
+col_idx = 1;
+figure;
+hold on;
+leg = cell(length(alphas) + 2, 1);
+
+for mu = [0.01 0.1]
+    w_est_tot = zeros(ORDER, N+1);
+    
+    for i=1:N_IT
+        [ w_est, ~ ] = lms_gass(wgn_mat(:, i), x_mat(:, i), ORDER, 'none', mu, 0, 0);
+        w_est_tot = w_est_tot + w_est;
+    end
+    
+    w_est_tot = w_est_tot/N_IT;
+    w_est_shift = 0.9 - w_est_tot(1, :);
+    plot(w_est_shift, 'Color', cols(col_idx, :));
+    leg{col_idx} = sprintf('LMS: \\mu=%0.3f', mu);
+    col_idx = col_idx + 1;
+end
+
+for alpha = alphas
+    w_est_tot = zeros(ORDER, N+1);
+    
+    for i = 1:N_IT
+        [ w_est, ~ ] = lms_gass(wgn_mat(:, i), x_mat(:, i), ORDER, 'ang', 1, 0.001, alpha);
+        w_est_tot = w_est_tot + w_est;
+    end
+    
+    w_est_tot = w_est_tot/N_IT;
+    w_est_shift = 0.9 - w_est_tot(1, :);
+    plot(w_est_shift, 'Color', cols(col_idx, :));
+    leg{col_idx} = sprintf('\\alpha = %0.3f', alpha);
+    col_idx = col_idx + 1;
+end
+
+xlabel('Iteration')
+ylabel('Weight Error ($\widehat{w}(n)$)', 'interpreter', 'latex')
+title('Ang \& Farhang method for varying $\alpha$ values, and LMS reference', 'interpreter', 'latex')
+xlim([1 N])
+legend(leg)
+common.set_graph_params
 
--- part_3/lms_gass.m
+++ part_3/lms_gass.m
@@ -6,36 +6,43 @@ w_est = zeros(order, N);
 error = zeros(N, order);
 x_est = zeros(N, order);
 
-step = zeros(order, 1);
+mu = mu*ones(N, 1);
+phi = zeros(order, 1);
 
+x_n = x(1:order);
 
-for n = order+2:N
+for n = order+1:N
+    %% Some oft used variables
+    
+    x_n_1 = x_n;
+    x_n = flipud(x(n-order+1:n));
+    
     %% Standard LMS Algorithm
-    x_est(n) = w_est(:, n)' * flipud(x(n-order:n-1));
+    x_est(n) = w_est(:, n)' * x_n;
     
     error(n) = d(n) - x_est(n);
     
-    w_est(:, n+1) = w_est(:, n) + mu*error(n)*flipud(x(n-order:n-1));
+    w_est(:, n+1) = w_est(:, n) + mu(n) * error(n) * x_n;
 
     %% GASS Algorithms
     % Benveniste
     if strcmp(step_method, 'benveniste') 
-    step = (eye(order) - mu*(flipud(x(n-order-1:n-2)) * flipud(x(n-order:n-1))'))*step + error(n-1)*flipud(x(n-order-1:n-2));
+    phi = (eye(order) - (mu(n-1) * (x_n_1 * x_n_1'))) * phi + error(n-1)*x_n_1;
     
     %Ang & Farhang
     elseif strcmp(step_method, 'ang')
-    step = alpha*step + error(n-1)*flipud(x(n-order:n-1));
+    phi = alpha*phi + error(n-1) * x_n_1;
     
     % Matthews & Xie
     elseif strcmp(step_method, 'matthews')
-    step = error(n-1)*flipud(x(n-order:n-1));
+    phi = error(n-1) * x_n_1;
     
     % Standard LMS
     else
-    step = zeros(order, 1);
+    phi = zeros(order, 1);
     end
     
-    mu = mu + (rho * error(n) * flipud(x(n-order:n-1))' * step);
+    mu(n+1) = mu(n) + (rho * error(n) * x_n_1' * phi);
 
 end
 
