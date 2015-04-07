a4c8e2c7c4e7b53f78048fdd53b133e0a98d359d
Thomas <txl11@imperial.ac.uk>
2015-04-08 00:12:47 +0100

    Sine gen function for 1.3 started

---
+++ part_1/SEASP_Part_1_3_sig_gen.m
@@ -0,0 +1,7 @@
+function [ sig ] = SEASP_Part_1_3_sig_gen(f0, a1, a2, phi1, phi2, alpha, N)
+%UNTITLED15 Summary of this function goes here
+%   Detailed explanation goes here
+
+sig = a1*sin()
+end
+
