07a4c00a88593510c092392e71e7573348d1909d
Thomas <txl11@imperial.ac.uk>
2015-04-08 10:02:44 +0100

    1.3.d done. although unable to compile on 'doze :(

--- part_1/SEASP_Part_1_3_d.m
+++ part_1/SEASP_Part_1_3_d.m
@@ -6,8 +6,9 @@ N = 256;
 
 OVERSAMP = 16;
 
-A2s = [1 0.1 0.01];
+A2s = [1 0.1 0.01 0.001];
 
+figure;
 for i = 1:length(A2s)
 
     sig = SEASP_Part_1_3_sig_gen(0.2, 1, A2s(i), 0, 0, 4, N, 0);
@@ -15,18 +16,18 @@ for i = 1:length(A2s)
     sig_f = fft(sig, OVERSAMP*N);
     w = limspace(1, N*OVERSAMP);
 
-    figure;
+    subplot(1, 4, i)
     plot(w, mag2db(abs(sig_f).^2));
     xlabel('Normalised Frequency');
-    ylabel('Power')
+    ylabel('Power (dB)')
     title(sprintf('Periodogram, $ a_2=%.2f $, $ \\alpha= %i$', A2s(i), 4));
-    xlim([0.54 0.64])
+    xlim([0.5 0.65])
 %     axis tight
     common.set_graph_params
 
 end
 
-% A2s = [ 1 0.75 ];
+figure;
 for i = 1:length(A2s)
 
     sig = SEASP_Part_1_3_sig_gen(0.2, 1, A2s(i), 0, 0, 12, N, 0);
@@ -34,12 +35,12 @@ for i = 1:length(A2s)
     sig_f = fft(sig, OVERSAMP*N);
     w = limspace(1, N*OVERSAMP);
 
-    figure;
+    subplot(1, 4, i)
     plot(w, mag2db(abs(sig_f).^2));
     xlabel('Normalised Frequency');
-    ylabel('Power')
+    ylabel('Power (dB)')
     title(sprintf('Periodogram, $ a_2=%.3f $, $ \\alpha= %i$', A2s(i), 12));
-    xlim([0.45 0.64])
+    xlim([0.45 0.65])
 %     axis tight
     common.set_graph_params
 
